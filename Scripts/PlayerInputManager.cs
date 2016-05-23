ee2c73337c24e2a1c4b4662585fbc3289c2ccd9e
Judith Amrhein <judith.amrhein@web.de>
2016-05-23 23:11:03 +0200

    Neuer Versuch

---
+++ Scripts/PlayerInputManager.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	private Rigidbody myRigidbody;
+
+	public float mySpeed = 10.0f;
+	public float myRotationSpeed = 100.0f;
+
+
+	private string myMovement = "Move";
+	private string myRotation = "Rotate";
+
+	private float myInputMov = 0.0f;
+	private float myInputRot;
+
+
+	void Start () {
+
+	}
+
+	public void Init() {
+		myRigidbody = GetComponent<Rigidbody> ();
+	}
+
+	private void Move() {
+		myInputMov = Input.GetAxis (myMovement);
+		Vector3 movement = transform.forward * myInputMov * mySpeed * Time.deltaTime;
+		myRigidbody.MovePos (myRigidbody.position + movement);
+	}
+
+	private void Rotate() {
+		myInputRot = Input.GetAxis (myRotation);
+		float rotate = myInputRot * myRotationSpeed * Time.deltaTime;
+		Quaternion turnRot = Quaternion.Euler (0f, rotate, 0f);
+		myRigidbody.MoveRot (myRigidbody.rotation * turnRot);
+	}
+
+	public void setPosition(Transform spawnPosition) {
+		myRigidbody.position = spawnPosition.position;
+	}
+		
+	void Update () {
+		Move ();
+		Rotate ();
+	}
+}
---
+++ Scripts/PlayerManager.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerManager : MonoBehaviour {
+    
+	PlayerInputManager myInputManager;
+	public Transform spawnPos;
+
+	public delegate void PlayerEvent(int currentPoints);
+	public static event PlayerEvent OnHealthChanged;
+
+	private int defaultHealth = 100;
+	public int myHealth;
+
+	public int health {
+		get {
+			return myHealth;
+		}
+
+		set { 
+			myHealth = value;
+		}
+	}
+
+	public void decreaseHealth(int value) {
+		myHealth -= value;
+		OnHealthChanged (myHealth);
+	}
+
+	public void SetDefaultHealth() {
+		myHealth = defaultHealth;
+		OnHealthChanged (myHealth);
+	}
+		
+	void Start () {
+		Init ();
+	}
+
+	void Init() {
+		myInputManager = gameObject.GetComponent<PlayerInputManager> ();
+		myInputManager.Init ();
+		myInputManager.setPosition (spawnPos);
+		OnHealthChanged(myHealth);
+	}
+
+	public void respawnPlayer() {
+		myInputManager.setPosition (spawnPos);
+	}
+}
